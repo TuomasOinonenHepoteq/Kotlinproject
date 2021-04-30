8016d217eb17110d4b0c2c835bc2841d8bda77bd
Tuomas Oinonen (Hepoteq Oy) <tuomas.oinonen@hepoteq.fi>
2021-04-30 15:04:51 +0300

    More tests

--- app/src/androidTest/java/com/hepoteq/kotlinproject/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/hepoteq/kotlinproject/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.hepoteq.kotlinproject", appContext.packageName)
+        assertEquals("com.hepoteq.kotlinproject1", appContext.packageName)
     }
 }
